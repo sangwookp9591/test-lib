1dc90c0086b36d121d247b386a8521926c458ee7
sangwookpark <sangwookp9591@gmail.com>
2025-07-30 15:30:53 +0900

    feat : form Password추가

--- src/app/features/auth/ui/LoginForm.tsx
+++ src/app/features/auth/ui/LoginForm.tsx
@@ -1,32 +1,39 @@
-// src/features/auth/ui/LoginForm.tsx
+// src/widgets/LoginForm/ui/LoginForm.tsx
 'use client';
 
 import { useState } from 'react';
 
 export function LoginForm() {
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        if (email === 'test@example.com') {
+
+        if (email === 'test@example.com' && password === '1234') {
             setMessage('로그인 성공');
         } else {
-            setMessage('로그인 실패');
+            setMessage('이메일 또는 비밀번호가 잘못되었습니다');
         }
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="email">이메일</label>
+            <input id="email" placeholder="이메일 입력" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+            <label htmlFor="password">비밀번호</label>
             <input
-                id="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="이메일 입력"
+                id="password"
+                placeholder="비밀번호 입력"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
             />
+
             <button type="submit">로그인</button>
+
             {message && <p>{message}</p>}
         </form>
     );
